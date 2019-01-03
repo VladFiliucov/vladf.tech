34ccffe03cf8d5d18c9b199d01a65a2ecc982737
Vlad Filiucov <vlad.filiucov@simplybusiness.co.uk>
2019-01-03 10:10:25 +0000

    Make title link to the root of the site.

--- src/components/TitleAndDescription.js
+++ src/components/TitleAndDescription.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const TitleAndDescription = ({ data }) => {
   const { title, description } = data.site.siteMetadata;
 
   return (
     <div>
-      <h2>{title}</h2>
+      <Link to='/' >
+        <h2>{title}</h2>
+      </Link>
       <p>{description}</p>
     </div>
   );

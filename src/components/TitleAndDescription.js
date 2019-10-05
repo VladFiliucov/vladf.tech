a39c80e8f6d324ebd5f43bcd54d07cfd657d9cfb
Vlad Filiucov <filiucov@mail.ru>
2019-10-05 11:15:50 +0100

    Example of adding a script

--- src/components/TitleAndDescription.js
+++ src/components/TitleAndDescription.js
@@ -13,6 +13,16 @@ const headerStyles = {
   backgroundImage: 'linear-gradient(to bottom left, #9ad8fd, #7eaee3)'
 }
 
+class Script extends React.Component {
+  componentDidMount() {
+    window.foo = () => `Vlad number ${5 - 4}`
+    console.log("Cookies", window.foo());
+  }
+  render() {
+    return null
+  }
+}
+
 const TitleAndDescription = ({ data }) => {
   const { title, description } = data.site.siteMetadata;
 
@@ -22,6 +32,7 @@ const TitleAndDescription = ({ data }) => {
         <Link to='/' >
           <img style={logoStyles} src={letterV} alt="Vlad Filiucov logo" />
         </Link>
+        <Script />
       </Header>
     </div>
   );

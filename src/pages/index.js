3022fa89b6f4f4edb51487304599764cf2444767
Vlad Filiucov <filiucov@mail.ru>
2019-10-06 10:44:28 +0100

    Make homepage layout more semantic

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,36 +2,42 @@ import React from "react"
 import { Helmet } from 'react-helmet';
 import Header from '../components/Header';
 import { graphql, Link } from 'gatsby';
+import './home.css';
 
 const Layout = ({data}) => {
   const { edges } = data.allMarkdownRemark;
   return (
-    <div>
+    <>
       <Helmet>
         <meta charSet="utf-8" />
         <title>Filiucov's personal blog</title>
         <link rel="canonical" href="http://vladf.tech" />
       </Helmet>
-      <Header />
-      {edges.map(edge => {
-        const { frontmatter } = edge.node;
+      <div className='main-container'>
+        <Header />
+          <section>
+            {edges.map(edge => {
+              const { frontmatter } = edge.node;
 
-        return (
-          <div key={frontmatter.path}>
-            {
-              <Link to={frontmatter.path} >
-                {frontmatter.title}
-              </Link>
-            }
-          </div>
-        );
-      })}
-      <div>
-        <Link to='/tags' >
-          Browse tags
-        </Link>
+              return (
+                <div key={frontmatter.path}>
+                  {
+                    <Link to={frontmatter.path} >
+                      {frontmatter.title}
+                    </Link>
+                  }
+                </div>
+              );
+            })}
+          </section>
+        <aside>
+          <Link to='/tags' >
+            Browse tags
+          </Link>
+        </aside>
+        <footer>Footer</footer>
       </div>
-    </div>
+    </>
   )
 }
 

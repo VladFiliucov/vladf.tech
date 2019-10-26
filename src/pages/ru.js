e54ddda2b7ec85c789092a7d508d85141ac3a171
Vlad Filiucov <filiucov@mail.ru>
2019-10-26 16:07:46 +0100

    Fix layout for ru locale on blog index page

--- src/pages/ru.js
+++ src/pages/ru.js
@@ -1,7 +1,12 @@
 import React from "react"
 import RuLayout from '../layouts/ru.js'
+import BlogList from '../templates/blogList';
 
-const RuHomePage = ({data, location}) => <RuLayout data={data} location={location} />;
+const RuHomePage = ({data, location}) => (
+  <RuLayout data={data} location={location}>
+    <BlogList edges={data.allMarkdownRemark.edges} />
+  </RuLayout>
+);
 
 export const query = graphql`
   query RuHomepageQuery {

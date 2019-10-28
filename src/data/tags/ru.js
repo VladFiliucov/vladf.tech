15b3931379310ac57941617fc0af39d3c9755bc3
Vlad Filiucov <filiucov@mail.ru>
2019-10-28 22:39:48 +0000

    Add tags index page and individual tag page

---
+++ src/data/tags/ru.js
@@ -0,0 +1,4 @@
+export default {
+  title: 'Тэги',
+  content: 'Совсем скоро...'
+}
--- src/templates/allTagsIndex.js
+++ src/templates/allTagsIndex.js
@@ -1,18 +1,26 @@
 import React from "react"
 import { graphql, Link } from 'gatsby';
+import EnLayout from '../layouts/en.js';
+import RuLayout from '../layouts/ru.js';
+import enTagsMessages from '../data/tags';
+import ruTagsMessages from '../data/tags/ru.js';
 
-const AllTagsTemplate = ({data, pageContext}) => {
-  const { tags } = pageContext;
+const AllTagsTemplate = ({data, pageContext, location}) => {
+  const { tags, lang } = pageContext;
+  const Layout = lang === 'ru' ? RuLayout : EnLayout;
+  const messages = lang === 'ru' ? ruTagsMessages : enTagsMessages;
+  const prefix = lang === 'ru' ? '/ru/tags/' : '/tags/';
 
   return (
-    <div>
-      <div>
+    <Layout data={data} location={location}>
+      <section>
+        <h2>{messages.title}</h2>
         <ul>
           {
             tags.map((tagName, index) => {
               return (
                 <li key={`tag-${index}`}>
-                  <Link to={`/tags/${tagName}`} >
+                  <Link to={`${prefix + tagName}`} >
                     {tagName}
                   </Link>
                 </li>
@@ -20,8 +28,8 @@ const AllTagsTemplate = ({data, pageContext}) => {
             })
           }
         </ul>
-      </div>
-    </div>
+      </section>
+    </Layout>
   )
 }
 

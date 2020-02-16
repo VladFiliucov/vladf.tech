83adac2d7cbdee9cd5177b651734b2234ec0af56
Vlad Filiucov <filiucov@mail.ru>
2020-02-16 16:43:05 +0000

    wip article

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,13 +4,17 @@
 import './src/pages/global.css'
 import React from "react"
 import { MDXProvider } from "@mdx-js/react"
+import { Typography } from 'antd';
 import Highlight, { defaultProps } from "prism-react-renderer";
 import Prism from 'prism-react-renderer/prism';
 (typeof global !== 'undefined' ? global : window).Prism = Prism;
 
 require('prismjs/components/prism-ruby');
 
+const { Text } = Typography;
+
 const components = {
+  p: props => <Text>{props.children}</Text>,
   pre: props => {
     const className = props.children.props.className || "";
     const matches = className.match(/language-(?<lang>.*)/);
--- src/components/Forms/RecaptchaForm.jsx
+++ src/components/Forms/RecaptchaForm.jsx
@@ -1,16 +1,84 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import Reaptcha from 'reaptcha';
 import { Formik } from 'formik';
 
-const TextField = () => (
-  <label>
-    Name
-    <input type="text" />
-  </label>
-);
+const RecaptchaForm = () => {
+  const recaptchaRef = useRef(null);
+  const [formSubmitted, setFormSubmitted] = useState(false);
 
-const RecaptchaForm = () => (
-    <TextField />
-  );
+  const submitForm = (values, token, setSubmitting) => {
+    const payload = {...values, recaptcha: token }
+
+    alert(JSON.stringify(payload, null, 2));
+    setSubmitting(false);
+    setFormSubmitted(true);
+  };
+
+  if (formSubmitted) return <div>Submitted</div>;
+
+  return (
+    <Formik
+      initialValues={{ email: '', recaptcha: '' }}
+      validateOnChange={false}
+      validate={values => {
+        return {}
+        // return recaptchaRef.current.execute().then(() => {
+        //   const errors = {};
+        //   if (!values.email) {
+        //     errors.email = 'Required';
+        //   }
+        //   console.log("CALLED THEEEEEEEN");
+
+        //   // recaptchaRef.current.execute();
+
+        //   return errors;
+        // })
+      }}
+      onSubmit={values => {
+        recaptchaRef.current.getResponse().then((token) => { console.log("TOKEN IS", token) });
+        console.log("HOPE THIS IS CALLED LATER");
+        // recaptchaRef.current.execute();
+      }}
+    >
+      {({
+        values,
+        errors,
+        touched,
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        setSubmitting,
+        isSubmitting,
+      }) => (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Email:
+            <input
+              type="email"
+              name="email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+          </label>
+          {errors.email && touched.email && errors.email}
+          <Reaptcha
+            ref={recaptchaRef}
+            size="invisible"
+            name="recaptcha"
+            sitekey=""
+            onVerify={(token) => {
+              console.log("GOT CALLED!", token);
+              // submitForm(values, token, setSubmitting)
+            }}
+          />
+          <button type="submit" disabled={isSubmitting}>
+            Submit
+          </button>
+        </form>
+      )}
+    </Formik>
+  )
+};
 
 export default RecaptchaForm;

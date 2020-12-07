af6edb33af127d2755938b466fa4161fd2901ef1
Vlad Filiucov <filiucov@mail.ru>
2020-12-07 21:13:20 +0000

    asdf

--- plugins/gatsby-source-pokemon/gatsby-node.js
+++
@@ -1,7 +0,0 @@
-async function pokemonToNode({ actions, createNodeId, createContentDigest }) {
-  console.log("GOT HERE YEHAA");
-}
-
-exports.sourceNodes = async props => {
-  await Promise.all([pokemonToNode(props)]);
-}

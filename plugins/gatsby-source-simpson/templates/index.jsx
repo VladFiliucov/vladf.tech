28f02d10d432d6f12f9ee1197c25bc27a2dbd0c9
Vlad Filiucov <filiucov@mail.ru>
2021-10-30 11:22:59 +0100

    most plugins are incompatible with Gatsby version 4

--- plugins/gatsby-source-simpson/templates/index.jsx
+++ plugins/gatsby-source-simpson/templates/index.jsx
@@ -4,40 +4,43 @@ import Img from "gatsby-image";
 
 const Template = ({ data: { allSimpson } }) => {
   return (
-    <div>
-      {allSimpson.edges.map(simpson => {
-          console.log(simpson.node.thumbnail.childImageSharp.fluid);
-        return (
-          <div>
-            <h2>{simpson.node.name}</h2>
-            <Img fluid={simpson.node.thumbnail.childImageSharp.fluid} />
-          </div>
-        )
-      })}
-    </div>
+    <div>Simplsons API no longer works</div>
   );
+  // return (
+  //   <div>
+  //     {allSimpson.edges.map(simpson => {
+  //         console.log(simpson.node.thumbnail.childImageSharp.fluid);
+  //       return (
+  //         <div>
+  //           <h2>{simpson.node.name}</h2>
+  //           <Img fluid={simpson.node.thumbnail.childImageSharp.fluid} />
+  //         </div>
+  //       )
+  //     })}
+  //   </div>
+  // );
 }
 
-export const query = graphql`
-  query SimpsonsPage {
-    allSimpson {
-      edges {
-        node {
-          thumbnail {
-            childImageSharp {
-              id
-              fluid {
-                ...GatsbyImageSharpFluid
-              }
-            }
-          }
-          episode
-          name
-          thumbnailUrl
-        }
-      }
-    }
-  }
-`;
+// export const query = graphql`
+//   query SimpsonsPage {
+//     allSimpson {
+//       edges {
+//         node {
+//           thumbnail {
+//             childImageSharp {
+//               id
+//               fluid {
+//                 ...GatsbyImageSharpFluid
+//               }
+//             }
+//           }
+//           episode
+//           name
+//           thumbnailUrl
+//         }
+//       }
+//     }
+//   }
+// `;
 
 export default Template;
